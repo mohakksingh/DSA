196b32809b78457d22ff012e976844b27a37ed11
quanta2k <72522871+quanta2k@users.noreply.github.com>
2024-02-26 23:20:39 +0530

    updated

---
+++ Sorting/BubbleSort.java
@@ -0,0 +1,33 @@
+package Sorting;
+
+import java.util.Arrays;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] arr={5,4,3,1,2};
+        bubble(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void bubble(int[] arr){
+        boolean swapped;
+        for(int i=0;i<arr.length;i++){
+            swapped=false;
+            for(int j=1;j<arr.length-i;j++){
+                if(arr[j]<arr[j-1]){
+                    swap(arr, j, j-1);
+                    swapped=true;
+                }
+            }
+            if(!swapped){
+                break;
+            }
+        }
+    } 
+
+    static void swap(int[] arr,int first,int second){
+        int temp=arr[first];
+        arr[first]=arr[second];
+        arr[second]=temp;
+    }
+}
---
+++ Sorting/SelectionSort.java
@@ -0,0 +1,36 @@
+package Sorting;
+
+import java.util.Arrays;
+
+public class SelectionSort {
+    public static void main(String[] args) {
+        int[] arr={1,5,24,5};
+        selection(arr);
+        System.out.println(Arrays.toString(arr  ));
+        
+    }
+
+    static public void selection(int[] arr){
+        for(int i=0;i<arr.length;i++){
+            int last=arr.length-i-1;
+            int maxIndex=getMaxIndex(arr, 0, last);
+            swap(arr, maxIndex,last);
+        }
+    }
+
+    static public int getMaxIndex(int[] arr,int start,int end){
+        int max=start;
+        for(int i=start;i<=end;i++){
+            if(arr[i]>arr[max]){
+                max=i;
+            }
+        }
+        return max;
+    }
+
+    static void swap(int[] arr,int first,int second){
+        int temp=arr[first];
+        arr[first]=arr[second];
+        arr[second]=temp;
+    }
+}
