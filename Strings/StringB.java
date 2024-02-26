f77ae45d9c15215385bf74a31bebb1eeb15647d9
quanta2k <72522871+quanta2k@users.noreply.github.com>
2024-02-27 02:06:15 +0530

    Strings Done

---
+++ Strings/StringB.java
@@ -0,0 +1,13 @@
+package Strings;
+
+public class StringB {
+    public static void main(String[] args) {
+        StringBuilder builder=new StringBuilder();
+        for (int i = 0; i < 26; i++) {
+            char ch=(char)('a'+i);
+            builder.append(ch);
+        }
+        builder.reverse();
+        System.out.println(builder.toString());
+    }
+}
---
+++ Strings/StringPrac.java
@@ -0,0 +1,10 @@
+package Strings;
+
+public class StringPrac {
+    public static void main(String[] args) {
+        String name="Mohak";
+        System.out.println(name);
+    }
+
+    
+}
