adf19e231c9f6b97f7290b25f55cd3e41b08b773
quanta2k <72522871+quanta2k@users.noreply.github.com>
2024-02-29 01:16:47 +0530

    AVL

--- Trees/Kunal/Main.java
+++ Trees/Kunal/Main.java
@@ -4,14 +4,12 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        // Scanner scanner=new Scanner(System.in);
-        // BinaryTree tree=new BinaryTree();
-        // tree.populate(scanner);
-        // tree.prettyDisplay();
+        AVL tree=new AVL();
 
-        BST tree=new BST();
-        int[] nums={5,2,7,1,4,6,9,8,3,10};
-        tree.populate(nums);
-        tree.display();
+        for(int i=0;i<1000;i++){
+            tree.insert(i);
+        }
+
+        System.out.println(tree.height());
     }
 }
