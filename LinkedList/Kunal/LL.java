77e54505af19a4d8e348186d519e946cf66f0780
quanta2k <72522871+quanta2k@users.noreply.github.com>
2024-05-08 03:07:50 +0530

    merge2 LL

--- LinkedList/Kunal/LL.java
+++ LinkedList/Kunal/LL.java
@@ -1,6 +1,7 @@
 package LinkedList.Kunal;
 
 import basicmaths.kunal.prime;
+import recursion.striver.nameprint;
 
 public class LL {
     
@@ -154,16 +155,48 @@ public class LL {
         tail=node;
         tail.next=null;
     }
-    public static void main(String[] args) {
-        LL list=new LL();
-        list.insertLast(1);
-        list.insertLast(1);
-        list.insertLast(2);
-        list.insertLast(3);
-        list.insertLast(3);
 
-        list.display();
-        list.duplicates();
-        list.display();
+    //merge
+    public static LL merge(LL first,LL second){
+        Node f=first.head;
+        Node s=second.head;
+
+        LL ans=new LL();
+
+        while (f!=null && s!=null) {
+            if(f.value<s.value){
+                ans.insertLast(f.value);
+                f=f.next;
+            }else{
+                ans.insertLast(s.value);
+                s=s.next;
+            }
+        }
+        while (f!=null) {
+            ans.insertLast(f.value);
+            f=f.next;
+        }
+        while (s!=null) {
+            ans.insertLast(s.value);
+            s=s.next;
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+        LL first=new LL();
+        LL second=new LL();
+
+        first.insertLast(1);
+        first.insertLast(3);
+        first.insertLast(5);
+
+        second.insertLast(1);
+        second.insertLast(2);
+        second.insertLast(9);
+        second.insertLast(14);
+
+        LL ans=LL.merge(first, second);
+        ans.display();
+        
     }
 }
