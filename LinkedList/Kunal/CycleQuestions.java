675473b258129e49b0384bd951016cff7f48c1b9
quanta2k <72522871+quanta2k@users.noreply.github.com>
2024-05-09 23:39:28 +0530

    middle of linked list

--- LinkedList/Kunal/CycleQuestions.java
+++ LinkedList/Kunal/CycleQuestions.java
@@ -97,7 +97,16 @@ public class CycleQuestions {
         }
         return ans;
     }
-
+    
+    public ListNode middleNode(ListNode head) {
+        ListNode slow=head;
+        ListNode fast=head;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        return slow;
+    }
 }
 
 
